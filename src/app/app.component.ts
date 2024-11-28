2ace327cea76a0c592283a508e5f76cf16d154ef
Lenin Jose <lenin.huaylla@gmail.com>
2024-11-28 16:06:44 -0500

    angular pannellum

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'viewer-360';
+export class AppComponent implements OnInit {
+  ngOnInit(): void {
+    (window as any).pannellum.viewer('panorama', {
+      "type": "equirectangular",
+      "panorama": "https://stcentenariodemos.blob.core.windows.net/public/360-images/3_ALAMEDA_CENTRAL.jpg",
+      "autoLoad": true,
+    });
+  }
 }
